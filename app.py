3d3e8c40172469fb74d8db88097fc0d492d04bf1
sbristol@usgs.gov <sbristol@usgs.gov>
2020-10-31 12:11:24 -0600

    Started person route

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/person/<person_id>", methods=['GET'])
+def lookup_person(person_id):
+    return person_id
